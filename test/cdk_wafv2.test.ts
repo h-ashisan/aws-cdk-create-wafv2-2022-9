0eca0e28e60625281c480471f1bf9ef5e1927930
h-ashisan <ashizawa.hiroaki@classmethod.jp>
2022-09-29 22:56:52 +0900

    Add cdk file

---
+++ test/cdk_wafv2.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkWafv2 from '../lib/cdk_wafv2-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk_wafv2-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkWafv2.CdkWafv2Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
